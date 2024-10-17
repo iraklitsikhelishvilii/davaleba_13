167b63cc3479fd3d058ea6229972a1d853f2238e
iraklitsikhelishvilii <11cixela11@gmail.com>
2024-10-17 20:14:32 +0400

    Lecture 13 Homework

---
+++ script.js
@@ -0,0 +1,12 @@
+const name = "ირაკლი";
+const lastname = "ციხელიშვილი";
+let age = 19;
+const Email = "iraklitsikhelishvili7@gmal.com";
+const phonenumber = "558 900 800";
+const არის_თუ_არა_სრულწლოვანი = true;
+console.log(name);
+console.log(lastname);
+console.log(age);
+console.log(Email);
+console.error(phonenumber)
+console.warn(არის_თუ_არა_სრულწლოვანი)
